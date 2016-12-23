726ddd0a16a5342d657d5560ca55d2d780911d52
Maria Lombardi <luciellamariella@gmail.com>
2016-12-23 19:51:26 +0100

    Create choices.m

---
+++ Adaptive Control/simulation/choices.m
@@ -0,0 +1,7 @@
+% Francesco Alderisio
+% f.alderisio@bristol.ac.uk
+% September 2015
+
+display(' ');
+display('[1] -> Human Leader (green), Avatar Follower (blue) with Mouse');
+display('[2] -> Human Leader (green), Avatar Follower (blue) with LeapMotion');
