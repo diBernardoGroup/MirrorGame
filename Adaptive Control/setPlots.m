cc5e8b478c935c7408553bd57e7efbb0a06fff8e
Maria Lombardi <luciellamariella@gmail.com>
2016-12-23 19:22:35 +0100

    added files to adaptive control folder

---
+++ Adaptive Control/setPlots.m
@@ -0,0 +1,20 @@
+% Francesco Alderisio
+% f.alderisio@bristol.ac.uk
+% September 2015
+
+%% Change default axes fonts.
+set(0,'DefaultAxesFontName', 'Palatino Linotype');
+set(0,'DefaultAxesFontSize', 25);
+
+%% Change default text fonts.
+set(0,'DefaultTextFontname', 'Palatino Linotype');
+set(0,'DefaultTextFontSize', 25);
+
+%% Set Latex as Default Interpreter
+set(0,'defaulttextinterpreter','latex');
+
+%% Set default figure position and size
+set(0, 'DefaultFigurePosition', [0 0 1000 700]);
+
+%% set default line thickness
+set(0,'defaultlinelinewidth',2);
